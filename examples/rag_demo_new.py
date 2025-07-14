1f6088d0bdc6946d5ba99d257e5897c4243604c4
GongCao <gcqw@berkeley.edu>
2025-07-14 16:51:22 +0800

    Update rag_demo_new.py

--- examples/rag_demo_new.py
+++ examples/rag_demo_new.py
@@ -66,7 +66,6 @@ async def main():
         )
         df = pd.read_excel('data.xlsx')
         answers = df['Content'].tolist()
-        answers = answers[0:2838]
         print(f"Answers list length: {len(answers)}")
         await rag.ainsert(answers)
         # 假设你的配置如下
