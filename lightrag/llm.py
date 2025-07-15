cc40a9d3a8094fb5e8b0cd79af79ac01f4143c7d
GongCao <gcqw@berkeley.edu>
2025-07-15 18:00:03 +0800

    Update llm.py

--- lightrag/llm.py
+++ lightrag/llm.py
@@ -127,8 +127,6 @@ async def req_emb(query):
     # time.sleep(0.5)
     current_time = str(datetime.now()).replace(' ', '-')
     print(f"query is {query}")
-    n = len(query[0])
-    print(f"The length is {n}")
     data = {
         "botId":"2025070910101415071",
         "bizUserId":"wangyichao_emb.wyc",
